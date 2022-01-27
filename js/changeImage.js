00bd8c8038d5e62e566d0e82651309de52fdaea0
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2022-01-27 17:21:55 +0200

    feat: add func of changing picture

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,6 +1,7 @@
-const changeImage = function () {
-  
-  console.log("yes");
-};
+let image = document.querySelector(".image");
 
-
+document.addEventListener("click", function(event) { 
+    if(event.target.classList.contains('sound-item')) {
+        image.src = `/assets/images/${event.target.dataset.image}.jpg`
+    }
+});
--- js/createListOfSounds.js
+++ js/createListOfSounds.js
@@ -4,10 +4,10 @@ const soundBtn = document.querySelector('.menu-item');
 const soundListContainer = document.querySelector('.sound-list-container')
 let soundList;
 
-const winterItems = [`<li class='winter-item'><button class='sound-item'>winter1</button></li>`];
-const springItems = [`<li class='spring-item'><button class='sound-item'>spring1</button></li>`];
-const summerItems = [`<li class='summer-item'><button class='sound-item'>summer1</button></li>`];
-const autumnItems = [`<li class='autumn-item'><button class='sound-item'>autumn1</button></li>`];
+const winterItems = [`<li class='winter-item'><button class='sound-item' data-image='winter1'>winter1</button></li><li class='winter-item'><button class='sound-item' data-image='winter2'>winter2</button></li><li class='winter-item'><button class='sound-item' data-image='winter3'>winter3</button></li>`];
+const springItems = [`<li class='spring-item'><button class='sound-item' data-image='spring1'>spring1</button></li>`];
+const summerItems = [`<li class='summer-item'><button class='sound-item' data-image='summer1'>summer1</button></li>`];
+const autumnItems = [`<li class='autumn-item'><button class='sound-item' data-image='autumn1'>autumn1</button></li>`];
 
 const changeSoundList = function (event) {
     if(soundList) {
@@ -25,11 +25,6 @@ const changeSoundList = function (event) {
         soundList.innerHTML = autumnItems;
     }
     soundListContainer.appendChild(soundList);
-    
-    const changeImgBtn = document.querySelector(".sound-item");
-    const image = document.querySelector(".image");
-    
-    changeImgBtn.addEventListener("click", changeImage);
 }
 
 soundsBtnList.addEventListener('click', changeSoundList);
